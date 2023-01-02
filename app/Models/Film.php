9fd189abad5175061ff8df8ddce1ed34c729614a
Jason Pratama <jason.pratama001@binus.ac.id>
2023-01-02 13:17:24 +0700

    Eloquent relation to category

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Film extends Model
 {
     use HasFactory;
+    public function category(){
+        return $this->hasOne(Category::class,'id','category_id');
+    }
 }
