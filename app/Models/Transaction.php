0ad6aa07314b667439539a6c3e63788313527551
Jason Pratama <jason.pratama001@binus.ac.id>
2023-01-04 00:39:26 +0700

    Added eloquent relationship and fillable field

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'user_id',
+        'type',
+        'grandtotal'
+    ];
+    public function detail(){
+        return $this->hasMany(TransactionDetail::class,'transaction_id');
+    }
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+    public function grandtotal($grandtotal){
+        $this->attributes['grandtotal']=$grandtotal;
+        self::save();
+    }
 }
