87d1ae5cb8a911dba3963c019df3129dff734747
Jason Pratama <jason.pratama001@binus.ac.id>
2023-01-03 23:54:29 +0700

    Added eloquent relationship and fillable field

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $fillable=['name'];
+    public function film(){
+        return $this->belongsToMany(Film::class,'category_id','id');
+    }
+
 }
